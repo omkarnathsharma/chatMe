c85342f1c5e16597f33de490fa68569dac5a5924
omkarnathsharma <43107337+omkarnathsharma@users.noreply.github.com>
2020-07-23 20:46:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const express=require('express')
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'))
+
+io.on('connection', (socket) =>{
+    console.log('connection established')
+    
+    socket.on('create or join', (room) => {
+        var myRoom = io.sockets.adapter.rooms[room] || {length:0}
+        var numClients= myRoom.length
+        if(numClients==0){
+            socket.join(room)
+            socket.emit('created', room)
+        }else if(numClients>=1){
+            socket.join(room)
+            socket.emit('joined', room)
+        }else {
+            socket.emit('full',room)
+        }
+    })
+
+    //relay on handlers
+    socket.on('ready', room => {
+        socket.broadcast.to(room).emit('ready')
+    })
+
+    socket.on('candidate', event => {
+        socket.broadcast.to(event.room).emit('candidate', event)
+    })
+    socket.on('offer', event => {
+        socket.broadcast.to(event.room).emit('offer', event.sdp)
+    })
+    socket.on('answer', event => {
+        socket.broadcast.to(event.room).emit('answer', event.sdp)
+    })
+
+
+})
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+ 
