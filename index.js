7863a1b8a120c53691687c822cb20e029ca346a9
omkarnathsharma <omkarnath@live.co.uk>
2020-07-24 20:20:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const io = require('socket.io')(http);
 var port =process.env.PORT || 5000
 app.use(express.static(__dirname + '/public'))
 
-// http.listen(port, () => {
-//     console.log('listening on *:', port);
-//   });
+http.listen(port, () => {
+    console.log('listening on *:', port);
+  });
