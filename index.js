634995430c1552c9035d20fbca2f0853ff50f034
omkarnathsharma <omkarnath@live.co.uk>
2020-07-24 19:46:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-var port =process.env.PORT || 3000
+var port =process.env.PORT || 5000
 app.use(express.static(__dirname + '/public'))
 
 http.listen(port, () => {
